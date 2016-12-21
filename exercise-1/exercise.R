36d467603cc81c22db51f7eb0f06fe2da8cd1ec7
pbrec <pbrand@ucdavis.edu>
2016-12-21 18:41:03 +0100

    completed all exercises

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,14 +2,29 @@
 
 # Write a function `AddThree` that adds 3 to an input value
 
+AddThree <- function(x){
+  
+  y<-x +3
+  
+  return(y)
+}
 
 # Create a variable `ten` by passing 7 to your `AddThree` function
 
+ten <- AddThree(7)
 
 # Write a function `FeetToMeters` that converts from feet to meters
 
+FeetToMeters <- function(x){
+    y<-x*0.3048
+  
+    return(y)
+}
 
 # Create a variable `height.in.feet` that is your height in feet
 
+height.in.feet<-5.97
 
 # Create a variable `height.in.meters` by passing `height.in.feet` to your `FeetToMeters` function
+
+height.in.meters <- FeetToMeters(height.in.feet)
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -3,18 +3,75 @@
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
 
+CompareLength<-function(x,y){
+  
+  z<-length(x)-length(y)
+  
+  return(abs(z)) #abs turns z to absolute value, to avoid negative values
+  
+}
 
 # Pass two vectors of different length to your `CompareLength` function
+a<-c(1:3)
+b<-c(1:7)
 
+CompareLength(b,a)
 
 # Write a function `DescribeDifference` that will return one of the following statements:
 # "Your first vector is longer by N elements"
 # "Your second vector is longer by N elements"
+DescribeDifference<-function(x,y){
+  
+  z<-length(x)-length(y)
+  
+  if(z>0){
+    answer<-paste("Your first vector is longer by", abs(z), "elements")  
+  } 
+  
+  else if(z<0){
+    answer<-paste("Your second vector is longer by", abs(z), "elements")
+    
+  }
+  else{
+    answer<-"Both vectors have the same length"
+  }
+  return(answer) 
+  
+}
+
 
 
 # Pass two vectors to your `DescribeDifference` function
+a<-c(1:3)
+b<-c(1:7)
 
-
+DescribeDifference(a,b)
 ### Bonus ###
 
 # Rewrite your `DescribeDifference` function to tell you the name of the vector which is longer
+
+DescribeDifference<-function(x,y){
+  
+  z<-length(x)-length(y)
+  
+  if(z>0){
+    name<-deparse(substitute(x))
+    answer<-paste("Vector", name, "is longer by", abs(z), "elements")  
+  } 
+  
+  else if(z<0){
+    name<-deparse(substitute(y))
+    answer<-paste("Vector", name, "is longer by", abs(z), "elements")
+    
+  }
+  else{
+    answer<-"Both vectors have the same length"
+  }
+  return(answer) 
+  
+}
+
+bee<-c(1:3)
+honey<-c(1:7)
+
+DescribeDifference(bee,honey)
